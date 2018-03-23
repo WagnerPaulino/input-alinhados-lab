abfa3f72a50066e0de36d6781bec455a489499ea
Wagner Paulino <wagnersantos9778@gmail.com>
2018-03-23 20:25:22 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { SegundoComponent } from './segundo/segundo.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrimeiroComponent,
+    SegundoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
